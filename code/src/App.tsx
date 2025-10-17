70e00af3aacfbf3adf92b65ff85ec299c0ce27ff
omkark-16 <155129274+omkark-16@users.noreply.github.com>
2025-10-17 18:23:10 +0530

    testimonial,feedback and pricing section added

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -7,6 +7,8 @@ import Trainers from './components/Trainers'
 import Facilities from './components/Facilities'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import PricingPlans from './components/PricingPlans'
+import Testimonials from './components/Testimonials'
 
 function App() {
 
@@ -34,6 +36,8 @@ function App() {
         <Facilities />
       </section>
 
+      <PricingPlans></PricingPlans>
+      <Testimonials></Testimonials>
       <section id='contact'>
         <Contact />
       </section>
